9b77f9a84e85379e14d932b0c83375377a2939c0
“zhaohb” <2996442165@qq.com>
2024-08-26 22:06:43 +0800

    fix

--- viteconfig.ts
+++ viteconfig.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         main: path.resolve(__dirname, 'index.html'),
       },
       output: {
-        entryFileNames: '[name].js',
+        entryFileNames: 'index.js',
         chunkFileNames: '[name].js',
         assetFileNames: '[name].[ext]',
       },
