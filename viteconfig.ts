d1c6f5f68f06ff8df498617df987f08cea300eed
zhaohubiao <zhaohubiao.zhb@sunyur.com>
2024-08-26 18:35:10 +0800

    init

---
+++ viteconfig.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import crx from 'vite-plugin-crx-mv3';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [crx({ manifest: path.resolve(__dirname, 'public/manifest.json') })],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+});
